42de40061657e902b05d5970f219b9c548de161f
IcaroJustino <icarojustinom@gmail.com>
2024-09-04 11:39:31 -0300

    ajuste no sessão about

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,5 +1,6 @@
 import { Usertype } from "../../config/types"
 import SocialButton from "../button/button";
+import { TypeAnimation } from "react-type-animation";
 import './styles.css';
 
 export default function AboutMe(Devdata: { user: Usertype }) {
@@ -21,9 +22,26 @@ export default function AboutMe(Devdata: { user: Usertype }) {
         <section className="about" id="about">
             <div className="devinfo">
                 <div className="usertext">
-                    <h1>{Devdata.user.name}</h1>
+                    <h1>Ola,meu nome é {Devdata.user.name}</h1>
+                    <span>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed once, initially
+                                'Desenvolvimento Fullstack',
+                                1000,
+                                'Analise de dados',
+                                1000,
+                                'Redes neurais',
+                                1000,
+                            ]}
+                            speed={10}
+                            style={{ fontSize: '2em' }}
+                            repeat={Infinity}
+                            deletionSpeed={10}
+                        />
+                    </span>
                     <p>
-                        {Devdata.user.description}
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos odit ut itaque dignissimos. Molestias soluta eligendi incidunt harum voluptas quas aliquam tenetur eaque, reiciendis ipsam ipsum adipisci omnis earum! Corporis.
                     </p>
                 </div>
                 <div className="links">
