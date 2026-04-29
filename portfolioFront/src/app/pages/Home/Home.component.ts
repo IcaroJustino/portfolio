55bf13ca6ba960159d830b128c3dca1b0119523b
IcaroJustino <icaromirandajustino@gmail.com>
2026-04-29 15:13:23 -0300

    feature:projects section

--- portfolioFront/src/app/pages/Home/Home.component.ts
+++ portfolioFront/src/app/pages/Home/Home.component.ts
@@ -4,6 +4,7 @@ import { AboutMeComponent } from '@components/AboutMeSection/Aboutme.component';
 import { ExperienceComponent } from '@components/ExperienceSection/Experience.component';
 import { LucideAngularModule } from 'lucide-angular';
 import { SkillsComponent } from '@components/SkillsSection/Skills.component';
+import { ProjectsComponent } from '@components/ProjectsSection/Projects.component';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,7 @@ import { SkillsComponent } from '@components/SkillsSection/Skills.component';
     AboutMeComponent,
     ExperienceComponent,
     SkillsComponent,
+    ProjectsComponent,
     LucideAngularModule,
   ],
   templateUrl: './Home.component.html',
